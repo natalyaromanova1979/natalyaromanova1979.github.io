c71b5410d856c94495ad7ef31ca2aad547975ad2
Natalya <n.o_1979@mail.ru>
2021-05-07 15:57:44 +0300

    07/05/2021

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/home/prev.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/home/next.png"></button>',
+        responsive:  [
+            {
+              breakpoint: 992,
+              settings: {
+                dots: true,
+                arrows: false,
+                speed: 500,
+                fade: true,
+              }
+              }
+          ] 
+    });
+  });
+      
