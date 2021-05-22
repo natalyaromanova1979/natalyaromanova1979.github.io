9faddeacfa0a062730d9f36ac749ea23946c2087
Natalya <n.o_1979@mail.ru>
2021-05-22 15:15:00 +0300

    22/05/2021

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/home/prev.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/home/next.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/home/prev.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/home/next.png"></button>',
+        dots: false,
         responsive:  [
             {
               breakpoint: 992,
