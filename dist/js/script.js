11b8da87c0025eca1570f60b4536892f002a709e
«Natalya» <n.o_1979@gmail.ru>
2022-01-29 21:25:35 +0300

    29/01/2022

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,49 @@
 
     new WOW().init();
 
+/*     $(document).ready(function(){
+        $('.carousel__inner').slick({
+            dots: true,
+            infinite: true, 
+            speed: 300,
+            slidesToShow: 1,
+            adaptiveHeight: true 
+          });
+      });
+          */ 
 
+ /*      var slider = tns({
+        container: '.carousel__inner',
+        items: 1, 
+        slideBy: 'page',
+        autoplay: false,
+        controlsText: [
+        
+        ]
+      
+      }); */
+      const slider = tns({
+        container: '.carousel__inner',
+        items: 1,
+        slideBy: 'page',
+        autoplay: false,
+        controls: false,
+        nav: false
+      });
+      
+
+      document.querySelector('.prev').addEventListener('click', function () {
+        slider.goTo('prev');
+      }); 
+      
+      document.querySelector('.next').addEventListener('click', function () {
+        slider.goTo('next');
+      }); 
+      
+      
+      
+      
+      
 
 
 
