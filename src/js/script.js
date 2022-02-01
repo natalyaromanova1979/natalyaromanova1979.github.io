92dde2f06326b62d9b99b4f277e4a3092f66b2fb
«Natalya» <n.o_1979@gmail.ru>
2022-02-01 16:28:02 +0300

    01/02/2022

--- src/js/script.js
+++ src/js/script.js
@@ -19,9 +19,35 @@
         slider.goTo('next');
       }); 
       
+
+    var marker = new google.maps.Marker({
+      position: {lat: 55.74, lng: 37.62},
+      map: map,
+      title: 'Legalizuem.ru',
+      icon: {
+        url: "../icons/footer/map_logo.png",
+        scaledSize: new google.maps.Size(64, 64)
+      }
+    });
+
+
+     $(window).scroll(function () {
+      if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+      } else {
+        $('.pageup').fadeOut();
+      }
+    });
+    
+    $("a[href^='#']").click(function(){
+      var _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+    }); 
+ 
+
       
-      
-      
+    
       
 
 
